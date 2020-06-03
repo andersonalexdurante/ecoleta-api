9ccd0cbba414b9da8f2fd8aca121149cb611d46e
Anderson Alex Durante <anderson.alexdurante@gmail.com>
2020-06-03 12:48:07 -0300

    cria migrations

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -5,7 +5,11 @@ const connection = knex({
     client: 'sqlite3',
     connection: {
         filename: path.resolve(__dirname, 'database.sqlite')
-    }
+    },
+    migrations: {
+        directory: path.resolve(__dirname, '/migrations')
+    },
+    useNullAsDefault: true
 })
 
 export default connection
